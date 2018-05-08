d24bc88a6ce8bf5964c41e23d20295148c833884
Brian Park <park-brian@users.noreply.github.com>
2018-05-08 11:27:21 -0400

    move navbar to component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HelpComponent } from './components/help/help.component';
+import { FormComponent } from './components/form/form.component';
+import { PreviewComponent } from './components/preview/preview.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HelpComponent,
+    FormComponent,
+    PreviewComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
