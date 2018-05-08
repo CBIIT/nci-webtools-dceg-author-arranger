c2f73a57be56d33ed01a5c13e5c75efb9fe52e80
Brian Park <park-brian@users.noreply.github.com>
2018-05-08 11:26:53 -0400

    add components

---
+++ src/app/components/preview/preview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'author-arranger-preview',
+  templateUrl: './preview.component.html',
+  styleUrls: ['./preview.component.css']
+})
+export class PreviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
