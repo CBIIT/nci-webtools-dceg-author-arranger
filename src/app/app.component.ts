df51c3a868c0c5caa205cee5231ee3d47ee0cb54
Brian Park <park-brian@users.noreply.github.com>
2018-05-08 11:22:58 -0400

    implement navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'author-arranger-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'author-arranger';
+  navbarCollapsed: boolean = true;
 }
