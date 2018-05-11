9205a54283dfbf877bdded483b63c2193cd31669
Brian Park <park-brian@users.noreply.github.com>
2018-05-11 18:17:09 -0400

    implement feedback

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -2,6 +2,7 @@ import { Component, Output, EventEmitter } from '@angular/core';
 import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 import { DragulaService } from 'ng2-dragula';
 import { ParserService } from '../../services/parser/parser.service';
+import { FormatParameters } from './form.models';
 
 @Component({
   selector: 'author-arranger-form',
@@ -10,25 +11,103 @@ import { ParserService } from '../../services/parser/parser.service';
 })
 export class FormComponent {
 
-  alerts = [];
-
-  form: FormGroup;
-
-  templateHeaders = ['Title', 'First', 'Middle', 'Last', 'Degree', 'Affiliations', 'Other']
-  headers = [];
-
   @Output()
-  change: EventEmitter<any> = new EventEmitter<any>();
+  change: EventEmitter<FormatParameters> = new EventEmitter<FormatParameters>();
+
+  alerts: {type: string, message: string}[] = [];
+
+  form: FormGroup = null;
+
+  defaultHeaders = ['Title', 'First', 'Middle', 'Last', 'Degree', 'Affiliations', 'Other']
+
+  defaultParameters: FormatParameters = {
+    file: {
+      filename: '',
+      files: null,
+      data: [],
+      headers: [],
+    },
+
+    author: {
+      fields: [
+        {
+          name: 'Title',
+          column: null,
+          addPeriod: true,
+          disabled: false,
+          index: 0,
+        },
+
+        {
+          name: 'First',
+          column: null,
+          abbreviate: false,
+          addPeriod: false,
+          disabled: false,
+          index: 1,
+        },
+
+        {
+          name: 'Middle',
+          column: null,
+          abbreviate: false,
+          addPeriod: false,
+          disabled: false,
+          index: 2,
+        },
+
+        {
+          name: 'Last',
+          column: null,
+          abbreviate: false,
+          addPeriod: false,
+          disabled: false,
+          index: 3,
+        },
+
+        {
+          name: 'Degree',
+          column: null,
+          addComma: false,
+          addPeriod: false,
+          disabled: false,
+          index: 4,
+        },
+
+        {
+          name: 'Other',
+          column: null,
+          addComma: false,
+          addPeriod: false,
+          disabled: false,
+          index: 5,
+        },
+      ],
+      separator: 'comma',
+      customSeparator: '',
+      labelPosition: 'superscript',
+    },
+
+    affiliation: {
+      column: null,
+      separator: 'comma',
+      customSeparator: '',
+      labelPosition: 'superscript',
+      labelStyle: 'numeric',
+    },
+  };
 
   constructor(
     private fb: FormBuilder,
     private ds: DragulaService,
     private ps: ParserService) {
+
     this.form = fb.group({
       file: fb.group({
         filename: '',
         files: null,
         data: [],
+        headers: [],
       }),
 
       author: fb.group({
@@ -62,7 +141,7 @@ export class FormComponent {
           fb.group({
             name: 'Last',
             column: null,
-            abbreviate: false,
+            addComma: false,
             addPeriod: false,
             disabled: false,
             index: 3,
@@ -92,6 +171,7 @@ export class FormComponent {
       }),
 
       affiliation: fb.group({
+        column: null,
         separator: 'comma',
         customSeparator: '',
         labelPosition: 'superscript',
@@ -99,221 +179,133 @@ export class FormComponent {
       }),
     });
 
-    this.form.valueChanges.subscribe(value => {
-      this.change.emit(value);
-    });
+    this.form.valueChanges.subscribe((value: FormatParameters) =>
+      this.change.emit(value));
 
     this.form.get('file.files').valueChanges.subscribe(async (files: FileList) => {
       try {
-
         this.alerts = [];
+        this.resetForm({file: {
+          filename: '',
+          files: files,
+          data: []
+        }});
 
         if (!files || files.length == 0) {
-          this.resetForm();
           return;
         }
 
         const file = files[0];
         const sheets = await ps.parse(file);
 
-        this.form.get('file.filename').patchValue(file.name);
-        let validHeaders = true;
-
-        let sheetData = []
-        this.headers = [];
-
-        const controls = (this.form.get('author.fields') as FormArray).controls;
-        controls.forEach(control => control.patchValue({column: null}));
-
-        // if there is more than one sheet
-        if (sheets.length > 1) {
-          const sheet = sheets.find(sheet => sheet.name == 'Template');
-          if (!sheet) {
-            this.alerts.push({
-              type: 'warning',
-              message: 'Please ensure the workbook contains a sheet named "Template".'
-            });
-            this.resetForm();
-            this.change.emit(this.form.value);
-            return;
-          } else {
-            sheetData = sheet.data;
-          }
-        } else if (sheets.length == 1) {
-          sheetData = sheets[0].data;
+        if (sheets.length == 0) {
+          throw({
+            type: 'danger',
+            message: 'The workbook contains no sheets.'
+          });
         }
 
-        if (sheetData.length <= 1) {
-          this.alerts.push({
+        // if there is only one sheet, use the first sheet
+        const sheet = sheets.length === 1
+          ? sheets[0]
+          : sheets.find(sheet => sheet.name == 'Authors');
+
+        if (!sheet) {
+          throw({
+            type: 'warning',
+            message: 'Please ensure the workbook contains an "Authors" sheet.'
+          });
+        }
+
+        if (sheet.data.length <= 1) {
+          throw({
             type: 'warning',
             message: 'No data could be parsed from the file.'
           });
-          this.resetForm();
-          this.change.emit(this.form.value);
-          return;
         }
 
-        this.headers = sheetData[0];
+        const fileHeaders = sheet.data.shift();
+        this.form.get('file.filename').patchValue(file.name);
+        this.form.get('file.headers').patchValue(fileHeaders);
+        this.form.get('file.data').patchValue({data: [...sheet.data]});
 
-        // attempt to match file headers with form fields
-        for (let header of this.headers) {
-          const controls = (this.form.get('author.fields') as FormArray).controls;
-          controls.forEach(control => control.patchValue({column: null}));
-          const control = controls.find((item: FormGroup) => item.value.name == header);
-          if (control) {
-            setTimeout(() => control.patchValue({column: this.headers.indexOf(header)}), 0);
-          } else if (!this.templateHeaders.includes(header)) {
-            validHeaders = false;
-          }
-        }
-
-        if (!validHeaders) {
-          let message = `The file contains headers not found in the template. Please ensure these headers are mapped properly in the fields below `;
-          if (!this.headers.includes('Affiliations'))
-            message += ', and the file contains an "Affiliations" header'
+        // attempt to map file headers to columns with the same name
+        if (!this.mapHeaders(fileHeaders)) {
           this.alerts.push({
             type: 'info',
-            message: message + '.'
+            message: 'The file contains headers not found in the template. Please ensure these headers are mapped properly in the fields below.'
           });
         }
 
-        if (!this.headers.includes('Affiliations')) {
-          return;
-        }
-
-        this.form.get('file').patchValue({data: sheetData});
       } catch (e) {
-        console.log(e);
-        this.alerts.push({
-          type: 'danger',
-          message: 'Please upload a valid excel workbook.'
-        });
+        if (e.type && e.message) {
+          this.alerts.push(e);
+        } else {
+          console.log(e);
+          this.alerts.push({
+            type: 'danger',
+            message: 'Please upload a valid excel workbook.',
+          });
+        }
         this.resetForm();
         this.change.emit(this.form.value);
       }
     });
 
-    ds.drop.subscribe(value => {
-      const group: HTMLElement = value[2];
-      const controls = (<FormArray>this.form.get('author.fields')).controls;
-
-      Array.from(group.children)
-        .forEach((node, index) => controls
+    // set form field indexes when dragged
+    ds.drop.subscribe((value: [string, HTMLElement, HTMLElement]) => {
+      const parent = value[2];
+      Array.from(parent.children)
+        .forEach((node, index) => (<FormArray>this.form.get('author.fields')).controls
           .find(control => control.value.name == node.getAttribute('data-name'))
           .patchValue({index}))
     });
-
   }
 
-  resetForm() {
-    this.headers = [];
+  mapHeaders(headers: string[]): boolean {
+    let validHeaders = true;
 
+    headers.forEach((header, column) => {
+
+      // update the affiliation column
+      if (header === 'Affiliations')
+        this.form.get('affiliation')
+          .patchValue({column})
+
+      // update author field mappings
+      else if (this.defaultHeaders.includes(header))
+        (<FormArray>this.form.get('author.fields')).controls
+          .find((item: FormGroup) => item.value.name == header)
+          .patchValue({column})
+
+      // otherwise, unknown headers must be manually mapped
+      else
+        validHeaders = false;
+    })
+
+    return validHeaders;
+  }
+
+  resetForm(parameters = {}) {
     this.form.reset({
-      file: {
-        filename: '',
-        files: null,
-        data: [],
-      },
-
-      author: {
-        fields: [
-          {
-            name: 'Title',
-            column: null,
-            addPeriod: true,
-            disabled: false,
-            index: 0,
-          },
-
-          {
-            name: 'First',
-            column: null,
-            abbreviate: false,
-            addPeriod: false,
-            disabled: false,
-            index: 1,
-          },
-
-          {
-            name: 'Middle',
-            column: null,
-            abbreviate: false,
-            addPeriod: false,
-            disabled: false,
-            index: 2,
-          },
-
-          {
-            name: 'Last',
-            column: null,
-            abbreviate: false,
-            addPeriod: false,
-            disabled: false,
-            index: 3,
-          },
-
-          {
-            name: 'Degree',
-            column: null,
-            addComma: false,
-            addPeriod: false,
-            disabled: false,
-            index: 4,
-          },
-
-          {
-            name: 'Other',
-            column: null,
-            addComma: false,
-            addPeriod: false,
-            disabled: false,
-            index: 5,
-          },
-        ],
-        separator: 'comma',
-        customSeparator: '',
-        labelPosition: 'superscript',
-      },
-
-      affiliation: {
-        separator: 'comma',
-        customSeparator: '',
-        labelPosition: 'superscript',
-        labelStyle: 'numeric',
-      },
+      ...this.defaultParameters,
+      ...parameters
     }, {emitEvent: false})
   }
 
-  getColumns() {
-    return ['Title', 'First', 'Middle', 'Last', 'Degree', 'Other']
-  }
-
-  useExample() {
-
-    const controls = this.form.controls;
-    const file = this.form.controls.file as FormGroup;
-    file.patchValue({
-      filename: 'AuthorArranger Example.xlsx',
-      data: [
-        ["Title","First","Middle","Last","Degree","Affiliations","Other"],
-        ["Dr","Mitchell","John","Machiela","ScD; MPH","Integrative Tumor Epidemiology Branch, Division of Cancer Epidemiology and Genetics, National Cancer Institute, Rockville, MD, USA"],
-        ["Mr","Geoffrey",null,"Tobias","BS","Office of the Directory, Division of Cancer Epidemiology and Genetics, National Cancer Institute, Rockville, MD, USA"],
-        ["Ms","Sue",null,"Pan","MS","Center for Biomedical Informatics and Information Technology, National Cancer Institude, Rockville, MD, USA"]
-      ],
+  useExample(): void {
+    this.resetForm({
+      file: {
+        filename: 'AuthorArranger Example.xlsx',
+        file: null,
+        headers: [...this.defaultHeaders],
+        data: [
+          ["Dr","Mitchell","John","Machiela","ScD; MPH","Integrative Tumor Epidemiology Branch, Division of Cancer Epidemiology and Genetics, National Cancer Institute, Rockville, MD, USA"],
+          ["Mr","Geoffrey",null,"Tobias","BS","Office of the Directory, Division of Cancer Epidemiology and Genetics, National Cancer Institute, Rockville, MD, USA"],
+          ["Ms","Sue",null,"Pan","MS","Center for Biomedical Informatics and Information Technology, National Cancer Institute, Rockville, MD, USA"]
+        ],
+      }
     });
-
-    this.headers = [...this.templateHeaders];
-    this.headers.forEach((header, index) => {
-      setTimeout(() => {
-        const controls = (<FormArray>this.form.get('author.fields')).controls;
-        let control = controls.find(control => control.value.name == header);
-        if (control) {
-          let column = this.headers.indexOf(header);
-          control.patchValue({column, index})
-        };
-      }, 0);
-    })
-
-
+    this.mapHeaders(this.defaultHeaders);
   }
 }
---
+++ src/app/components/form/form.models.ts
@@ -0,0 +1,31 @@
+export interface FormatParameters {
+    file: {
+        filename: string,
+        files: FileList | null,
+        data: string[][],
+        headers: string[],
+    };
+
+    author: {
+        fields: {
+            name: string;
+            column: number | null;
+            index: number;
+            abbreviate?: boolean;
+            addPeriod?: boolean;
+            addComma?: boolean;
+            disabled?: boolean;
+        }[];
+        separator: string;
+        customSeparator: string;
+        labelPosition: string;
+    };
+
+    affiliation: {
+        column: number | null;
+        separator: string;
+        customSeparator: string;
+        labelPosition: string;
+        labelStyle: string;
+    };
+}
