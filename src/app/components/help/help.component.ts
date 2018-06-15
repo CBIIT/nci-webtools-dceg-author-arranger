2afb43fb307631ada15f9c87c2680d874b02c5a9
Brian Park <park-brian@users.noreply.github.com>
2018-06-15 16:40:55 -0400

    format code

--- src/app/components/help/help.component.ts
+++ src/app/components/help/help.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'author-arranger-help',
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.css']
 })
-export class HelpComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class HelpComponent {}
